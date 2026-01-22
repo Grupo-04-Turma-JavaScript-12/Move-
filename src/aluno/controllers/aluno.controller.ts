a510187cbfc2ace60a02f72efb64f3b68185ad05
Jacqueline Cardeal <jacquelinecardeal@hotmail.com>
2026-01-22 11:20:07 -0300

    :sparkles: feat: adicionada classe alunoController

---
+++ src/aluno/controllers/aluno.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, HttpCode, HttpStatus, Get, Param, ParseIntPipe, Post, Body, Delete, Put } from "@nestjs/common";
+import { AlunoService } from "../services/aluno.service";
+import { Aluno } from "../entities/aluno.entity";
+
+
+@Controller("/alunos")
+export class AlunoController {
+    constructor(private readonly alunoService: AlunoService) { }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Aluno[]> {
+        return this.alunoService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Aluno> {
+        return this.alunoService.findById(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() aluno: Aluno): Promise<Aluno> {
+        return this.alunoService.create(aluno);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() aluno: Aluno): Promise<Aluno> {
+        return this.alunoService.update(aluno);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.alunoService.delete(id);
+    }
+
+}
