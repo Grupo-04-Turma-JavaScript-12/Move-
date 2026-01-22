88803ff489a1b91f80a876c9a2de044ca7020107
Bruna Melo <bmelo.silva25@gmail.com>
2026-01-22 10:55:45 -0300

    Implementação das Classes Controller/Service

---
+++ src/categoria/services/categoria.service.ts
@@ -0,0 +1,66 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { Categoria } from '../entities/categoria.entity';
+
+@Injectable()
+export class CategoriaService {
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
+
+  async findAll(): Promise<Categoria[]> {
+    return await this.categoriaRepository.find({
+      relations: {
+        alunos: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Categoria> {
+    let categoria = await this.categoriaRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        alunos: true,
+      },
+    });
+
+    if (!categoria)
+      throw new HttpException(
+        'Categoria não encontrado!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return categoria;
+  }
+
+  async findAllByDescricao(descricao: string): Promise<Categoria[]> {
+    return await this.categoriaRepository.find({
+      where: {
+        descricao: ILike(`%${descricao}%`),
+      },
+      relations: {
+        alunos: true,
+      },
+    });
+  }
+
+  async create(Categoria: Categoria): Promise<Categoria> {
+    return await this.categoriaRepository.save(Categoria);
+  }
+
+  async update(categoria: Categoria): Promise<Categoria> {
+    await this.findById(categoria.id);
+
+    return await this.categoriaRepository.save(categoria);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return await this.categoriaRepository.delete(id);
+  }
+}
